6c7b2db4d4c22e39ef1eb74348d4c276bbe38a4e
James Walker <walkah@walkah.net>
2013-11-25 23:16:21 -0500

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "walkah@walkah.net"
 license          "All rights reserved"
 description      "Installs/Configures minecraft"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.1.0"
 recipe           "minecraft", "Installs minecraft server"
 
 supports "ubuntu"
